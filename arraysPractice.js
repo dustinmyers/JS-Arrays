9a922ac77806da9879aede63cbe5deb1a468b960
Dustin Myers <dustint314@gmail.com>
2015-04-03 16:19:30 -0600

    Done with all problems up to random number prob. Will finish the rest this weekend.

--- arraysPractice.js
+++ arraysPractice.js
@@ -62,14 +62,26 @@ var evenFinder = function(nums) {
 
 var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 var evens = [];
-var odds = []
+var odds = [];
 //Write a function called divider that is given three arguments, nums, evens, and odds.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
 
 
-  //Code Here
+var divider = function(nums, evens, odds) {
+  for (var i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    }
+    else {
+      odds.push(nums[i]);
+    }
+  };
+  var returnedArray = [evens, odds]
+  return returnedArray;
+}
 
+divider(nums, evens, odds);
 
 //Next Problem
 
@@ -80,8 +92,17 @@ var getRandomArbitrary = function() {
 var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above your given a function that will return a random number between 0 and 30, and an array full or numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
-
+var finder = function() {
+  var randomNumber = function() {
+    return Math.floor(Math.random() * (30 - 0) + 0);  
+  }
+    if (numbers.indexOf(randomNumber()) > 0) {
+      return true;
+    } 
+    else {
+      return false;
+    }
+}
 
 //Next problem
 
